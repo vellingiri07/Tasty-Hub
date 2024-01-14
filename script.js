26b2ee7fa6434b6fc1c544816bae91965a67fd4c
vellingiri07 <vellingiri1243@gmail.com>
2024-01-14 20:26:05 +0530

    bannerPage html added

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const banner = document.querySelector('.banner');
 
+const bannerPageProps = document.querySelector('.bannerPageContainer')
 
 function randomMeal(props) {
-    let data = props.meals[0]
-   banner.innerHTML =  `<div class="bannerContentContainer">
-   <h1 ">${data.strMeal}</h1>
-   <button class="bannerButton">Click to Cook</button>
+   let data = props.meals[0]
+   const mealImage = data.strMealThumb
+   const mealName = data.strMeal
+   banner.innerHTML = `<div class="bannerContentContainer">
+   <h1 ">${mealName}</h1>
+   <button class="bannerButton" onclick="openBannerPage()">Click to Cook</button>
 </div>
 <div>
-   <img src=${data.strMealThumb} alt="photo">
+   <img src=${mealImage} alt="photo">
 </div>`
-   
-   //`<div>
-//     <img src=${data.strMealThumb} alt="">
-//     <h1 class="bannerContent">${data.strMeal}</h1>
-//     <button class="bannerContent bannerButton">Click to Cook</button>
 
-// </div>`
-    console.log('Function ', data)
+   console.log('Function ', data)
+}
+
+// Functions for Banner Page
+function openBannerPage(){
+   window.location.href="bannerPage.html"
 }
 
 
 
+
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then((data) => randomMeal(data))
-    .catch(erro => console.log('error da punda', erro))
+   .then(res => res.json())
+   .then((data) => { randomMeal(data) })
+   .catch(error => console.log('error', error))
