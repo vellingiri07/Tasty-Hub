60a52c0b98013e9d53a71b99263cd1246a7c6d38
vellingiri07 <vellingiri1243@gmail.com>
2024-01-14 14:29:59 +0530

    photos folder added

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const banner=document.querySelector('.banner');
+const banner = document.querySelector('.banner');
 
 
-function randomMeal(props){
+function randomMeal(props) {
     let data = props.meals[0]
-    banner.innerHTML = `<div>
-    <img src=${data.strMealThumb} alt="">
-    <h1 class="bannerContent">${data.strMeal}</h1>
-    <button class="bannerContent bannerButton">Click to Cook</button>
-
+   banner.innerHTML =  `<div class="bannerContentContainer">
+   <h1 ">${data.strMeal}</h1>
+   <button class="bannerButton">Click to Cook</button>
+</div>
+<div>
+   <img src=${data.strMealThumb} alt="photo">
 </div>`
-    console.log('Function ',data)
+   
+   //`<div>
+//     <img src=${data.strMealThumb} alt="">
+//     <h1 class="bannerContent">${data.strMeal}</h1>
+//     <button class="bannerContent bannerButton">Click to Cook</button>
+
+// </div>`
+    console.log('Function ', data)
 }
 
 
 
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-.then(res=>res.json())
-.then((data)=>randomMeal(data))
-.catch(erro=>console.log('error da punda', erro))
+    .then(res => res.json())
+    .then((data) => randomMeal(data))
+    .catch(erro => console.log('error da punda', erro))
