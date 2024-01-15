9a82af07516c19fbb0d7263d9e34b52fc4d95f0f
vellingiri <vellingiri1243@gmail.com>
2024-01-15 12:34:52 +0530

    recipes cards fetched

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const banner = document.querySelector('.banner');
 
 const bannerPageProps = document.querySelector('.bannerPageContainer')
 
+const recipesContainer = document.querySelector('.recipesContainer')
+
 function randomMeal(props) {
    let data = props.meals[0]
    const mealImage = data.strMealThumb
    const mealName = data.strMeal
    banner.innerHTML = `<div class="bannerContentContainer">
    <h1 ">${mealName}</h1>
-   <button class="bannerButton" onclick="openBannerPage()">Click to Cook</button>
+   <h2 class="bannerButton" onclick="openBannerPage()">Click to Cook</h2>
 </div>
 <div>
    <img src=${mealImage} alt="photo">
@@ -17,9 +19,23 @@ function randomMeal(props) {
    console.log('Function ', data)
 }
 
+function recipesCards(recipesProps) {
+   let recipesData = recipesProps.meals[0]
+
+   const recipesImages = recipesData.strMealThumb
+   const recipesName = recipesData.strMeal
+
+   recipesContainer.innerHTML = `  <div class="recipesCards">
+   <img src=${recipesImages} alt="${recipesName}Photo">
+   <span class="recipesNames">${recipesName}</span>
+   </div>`
+}
+
+
+
 // Functions for Banner Page
-function openBannerPage(){
-   window.location.href="bannerPage.html"
+function openBannerPage() {
+   window.location.href = "bannerPage.html"
 }
 
 
@@ -27,5 +43,5 @@ function openBannerPage(){
 
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
    .then(res => res.json())
-   .then((data) => { randomMeal(data) })
+   .then((data) => { randomMeal(data), recipesCards(data) })
    .catch(error => console.log('error', error))
