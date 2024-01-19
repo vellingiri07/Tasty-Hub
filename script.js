19b8372f44ff09d2df8deaf191e85cdbc600a6e6
vellingiri <vellingiri1243@gmail.com>
2024-01-19 16:20:51 +0530

    updated

--- script.js
+++ script.js
@@ -1,136 +1,137 @@
-const banner = document.querySelector('.banner');
-
-const bannerPageProps = document.querySelector('.bannerPageContainer')
-
-const recipesContainer = document.querySelector('.recipesContainer')
+// const banner = document.querySelector('.banner');
 
 
-
-// Function For Banner Random Meals
-function randomMeal(props) {
-   let data = props.meals[0]
-   const mealImage = data.strMealThumb
-   const mealName = data.strMeal
-   banner.innerHTML = `<div class="bannerContentContainer">
-   <h1 ">${mealName}</h1>
-   <h2 class="bannerButton" onclick="openBannerPage()">Click to Cook</h2>
-</div>
-<div>
-   <img src=${mealImage} alt="photo">
-</div>`
-
-   console.log('Function ', data)
-}
-
-
-// For Random Meal
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-   .then(res => res.json())
-   .then((data) => { randomMeal(data) })
-   .catch(error => console.log('error', error))
-
-
-// Functions for Banner Page
-function openBannerPage() {
-   window.location.href = "bannerPage.html"
-}
+// const recipesContainer = document.querySelector('.recipesContainer')
 
 
 
-// For Random Recipes Cards 
+// // Function For Banner Random Meals
+// function randomMeal(props) {
+//    let data = props.meals[0]
+//    const mealImage = data.strMealThumb
+//    const mealName = data.strMeal
+//    banner.innerHTML = `<div class="bannerContentContainer">
+//    <h1 ">${mealName}</h1>
+//    <h2 class="bannerButton" onclick="openBannerPage()">Click to Cook</h2>
+// </div>
+// <div>
+//    <img src=${mealImage} alt="photo">
+// </div>`
 
-let temp = '';
-
-function recipesCards(recipesProps) {
-   let recipesData = recipesProps.meals[0]
-
-   const recipesImages = recipesData.strMealThumb
-   const recipesName = recipesData.strMeal
-
-   temp += `  <div class="recipesCards">
-      <img src=${recipesImages} alt="${recipesName}Photo">
-      <span class="recipesNames">${recipesName}</span>
-      </div>`
-}
-
-
-for (let i = 0; i < 40; i++) {
-   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then(res => res.json())
-      .then((data) => { recipesCards(data), recipesContainer.innerHTML = temp })
-      .catch(error => console.log('error', error))
-}
+//    console.log('Function ', data)
+// }
 
 
+// // For Random Meal
+// fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+//    .then(res => res.json())
+//    .then((data) => { randomMeal(data) })
+//    .catch(error => console.log('error', error))
 
-// Categories vise Recipes Card
 
-function vegRecipes() {
+// // Functions for Banner Page
+// function openBannerPage() {
+//    window.location.href = "bannerPage.html"
+// }
 
-}
 
 
-function nonVegRecipes() {
-   recipesContainer.innerHTML = ` <div class="recipesCards">
-   <img src="./Photos/Palmyra.jpeg" alt="">
-   <span class="recipesNames">Palmyra Juice</span>
-</div>`
-}
+// // For Random Recipes Cards 
 
-let seaFood = ''
+// let temp = '';
 
-function seaFoodRecipes() {
+// function recipesCards(recipesProps) {
+//    let recipesData = recipesProps.meals[0]
 
+//    const recipesImages = recipesData.strMealThumb
+//    const recipesName = recipesData.strMeal
+
+//    temp += `  <div class="recipesCards">
+//       <img src=${recipesImages} alt="${recipesName}Photo">
+//       <span class="recipesNames">${recipesName}</span>
+//       </div>`
+// }
+
+
+// for (let i = 0; i < 40; i++) {
+//    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+//       .then(res => res.json())
+//       .then((data) => { recipesCards(data), recipesContainer.innerHTML = temp })
+//       .catch(error => console.log('error', error))
+// }
+
+
+
+// // Categories vise Recipes Card
+
+// // function vegRecipes() {
+
+// // }
+
+
+// // function nonVegRecipes() {
+// //    recipesContainer.innerHTML = ` <div class="recipesCards">
+// //    <img src="./Photos/Palmyra.jpeg" alt="">
+// //    <span class="recipesNames">Palmyra Juice</span>
+// // </div>`
+// // }
+
+
+
+// // function seaFoodRecipes() {
+
+// //    let seaFood = ''
+// //    function getSeaFoodRecipes(seaProps) {
+// //       let data = seaProps.meals[0]
+
+// //       const seaFoodMeal = data.strMeal
+// //       const seaMealImage = data.strMealThumb
+
+// //       seaFood += ` <div class="recipesCards">
+// //       <img src=${seaMealImage} alt="">
+// //       <span class="recipesNames">${seaFoodMeal}</span>
+// //    </div>`
+
+// //       console.log('veg', data)
+// //    }
+
+// //    for (let i = 0; i <= 20; i++) {
+// //       fetch('https://themealdb.com/api/json/v1/1/filter.php?c=seafood')
+// //          .then(res => res.json())
+// //          .then((data) => { getSeaFoodRecipes(data), recipesContainer.innerHTML = seaFood })
+// //          .catch(error => console.log('error', error))
+// //    }
    
+// // }
 
-   function getSeaFoodRecipes(seaProps) {
-      let data = seaProps.meals[0]
+// // function soupRecipes() {
+// //    recipesContainer.innerHTML = ` <div class="recipesCards">
+// //    <img src="./Photos/PineApple.png" alt="">
+// //    <span class="recipesNames">Pineapple Juice</span>
+// // </div>`
+// // }
 
-      const seaFoodMeal = data.strMeal
-      const seaMealImage = data.strMealThumb
+// // function snacksRecipes() {
+// //    recipesContainer.innerHTML = ` <div class="recipesCards">
+// //    <img src="./Photos/Hibiscus.jpeg" alt="">
+// //    <span class="recipesNames">Hibiscus Juice</span>
+// // </div>`
+// // }
 
-      seaFood += ` <div class="recipesCards">
-      <img src=${seaMealImage} alt="">
-      <span class="recipesNames">${seaFoodMeal}</span>
-   </div>`
+// // function saladsRecipes() {
+// //    recipesContainer.innerHTML = ` <div class="recipesCards">
+// //    <img src="./Photos/Orange.png" alt="">
+// //    <span class="recipesNames">Orange Juice</span>
+// // </div>`
+// // }
 
-      console.log('veg', data)
-   }
+// // function dessertsRecipes() {
+// //    recipesContainer.innerHTML = ` <div class="recipesCards">
+// //    <img src="./Photos/dhal rice.jpg" alt="">
+// //    <span class="recipesNames">Dhal rice</span>
+// // </div>`
+// // }
 
-   for (let i = 0; i <= 20; i++) {
-      fetch('https://themealdb.com/api/json/v1/1/filter.php?c=seafood')
-         .then(res => res.json())
-         .then((data) => { getSeaFoodRecipes(data), recipesContainer.innerHTML = seaFood })
-         .catch(error => console.log('error', error))
-   }
-   
-}
 
-function soupRecipes() {
-   recipesContainer.innerHTML = ` <div class="recipesCards">
-   <img src="./Photos/PineApple.png" alt="">
-   <span class="recipesNames">Pineapple Juice</span>
-</div>`
-}
 
-function snacksRecipes() {
-   recipesContainer.innerHTML = ` <div class="recipesCards">
-   <img src="./Photos/Hibiscus.jpeg" alt="">
-   <span class="recipesNames">Hibiscus Juice</span>
-</div>`
-}
-
-function saladsRecipes() {
-   recipesContainer.innerHTML = ` <div class="recipesCards">
-   <img src="./Photos/Orange.png" alt="">
-   <span class="recipesNames">Orange Juice</span>
-</div>`
-}
-
-function dessertsRecipes() {
-   recipesContainer.innerHTML = ` <div class="recipesCards">
-   <img src="./Photos/dhal rice.jpg" alt="">
-   <span class="recipesNames">Dhal rice</span>
-</div>`
-}
 
